6a102ef688a8747ad89b474c670d7ad42c918a39
Trinadh <53462309+tr1nadh@users.noreply.github.com>
2023-12-12 19:45:23 +0530

    Initial commit

---
+++ src/main/java/com/example/twinfileshare/TWinFileShareApplication.java
@@ -0,0 +1,13 @@
+package com.example.twinfileshare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TWinFileShareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TWinFileShareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/twinfileshare/TWinFileShareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.twinfileshare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TWinFileShareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
