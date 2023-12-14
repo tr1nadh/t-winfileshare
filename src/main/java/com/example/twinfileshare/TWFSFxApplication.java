94b83224a90d5c6b23fc6c3227d81d7017fa6880
Trinadh <53462309+tr1nadh@users.noreply.github.com>
2023-12-14 12:32:51 +0530

    Added JavaFx Configuration

---
+++ src/main/java/com/example/twinfileshare/TWFSFxApplication.java
@@ -0,0 +1,21 @@
+package com.example.twinfileshare;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import org.springframework.context.annotation.Configuration;
+
+import java.io.IOException;
+
+@Configuration
+public class TWFSFxApplication {
+
+    public static void loadView(Stage stage) throws IOException {
+        FXMLLoader loader = new FXMLLoader(TWFSFxApplication.class.getResource("/Main.fxml"));
+        loader.setControllerFactory(TWinFileShareApplication.getApplicationContext()::getBean);
+        Parent root = loader.load();
+        stage.setScene(new Scene(root));
+        stage.show();
+    }
+}
--- src/main/java/com/example/twinfileshare/TWinFileShareApplication.java
+++ src/main/java/com/example/twinfileshare/TWinFileShareApplication.java
@@ -1,13 +1,36 @@
 package com.example.twinfileshare;
 
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.stage.Stage;
+import lombok.Getter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
-public class TWinFileShareApplication {
+public class TWinFileShareApplication extends Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TWinFileShareApplication.class, args);
+	@Getter
+	private static ConfigurableApplicationContext applicationContext;
+
+	@Override
+	public void init() {
+		applicationContext = SpringApplication.run(TWinFileShareApplication.class);
 	}
 
+	@Override
+	public void stop() {
+		applicationContext.close();
+		Platform.exit();
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage stage) throws Exception {
+		TWFSFxApplication.loadView(stage);
+	}
 }
