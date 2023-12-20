9814f040d9f70324eaacd779adc664d200ce767e
Trinadh <53462309+tr1nadh@users.noreply.github.com>
2023-12-20 11:10:10 +0530

    Added choice box to choose connected google drive account

--- src/main/java/com/example/twinfileshare/GoogleUserCREDJPA.java
+++ src/main/java/com/example/twinfileshare/GoogleUserCREDJPA.java
@@ -1,6 +1,12 @@
 package com.example.twinfileshare;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface GoogleUserCREDJPA extends JpaRepository<GoogleUserCRED, Long> {
+
+    @Query(nativeQuery = true, value = "SELECT email FROM google_user_cred")
+    List<String> findAllEmail();
 }
--- src/main/java/com/example/twinfileshare/fx/controller/MainController.java
+++ src/main/java/com/example/twinfileshare/fx/controller/MainController.java
@@ -1,13 +1,20 @@
 package com.example.twinfileshare.fx.controller;
 
+import com.example.twinfileshare.GoogleUserCREDJPA;
 import com.example.twinfileshare.TWinFileShareApplication;
 import com.example.twinfileshare.service.GoogleAuthorizationService;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ChoiceBox;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
 @Controller
-public class MainController {
+public class MainController implements Initializable {
 
     @Autowired
     private TWinFileShareApplication tWinFileShareApplication;
@@ -18,5 +25,16 @@ public class MainController {
     public void connectGoogleDrive(ActionEvent event) {
         var hostServices = tWinFileShareApplication.getHostServices();
         hostServices.showDocument(googleAuthorizationService.getGoogleSignInURL());
+    }
+
+    @FXML
+    private ChoiceBox<String> accountChoiceBox;
+
+    @Autowired
+    private GoogleUserCREDJPA googleUserCREDJPA;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        accountChoiceBox.getItems().addAll(googleUserCREDJPA.findAllEmail());
     }
 }
