1f7d48b092c8ab99d65299599e36e9005d80f70a
Trinadh <53462309+tr1nadh@users.noreply.github.com>
2023-12-24 14:11:01 +0530

    Added a profile for web boot main class

--- src/main/java/com/example/twinfileshare/TwinFileShareSpringBootApplication.java
+++ src/main/java/com/example/twinfileshare/TwinFileShareSpringBootApplication.java
@@ -2,7 +2,9 @@ package com.example.twinfileshare;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Profile;
 
+@Profile("dev")
 @SpringBootApplication
 public class TwinFileShareSpringBootApplication {
 
