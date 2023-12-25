17385ef710d9bfd8a5732e9a9a8763bb5786a79c
Trinadh <53462309+tr1nadh@users.noreply.github.com>
2023-12-25 14:32:26 +0530

    initial commit

---
+++ src/main/java/com/example/twinfileshare/service/utility/UserScopes.java
@@ -0,0 +1,7 @@
+package com.example.twinfileshare.service.utility;
+
+public class UserScopes {
+
+    public static final String  USER_INFO_EMAIL = "https://www.googleapis.com/auth/userinfo.email";
+    public static final String USER_INFO_PROFILE = "https://www.googleapis.com/auth/userinfo.profile";
+}
