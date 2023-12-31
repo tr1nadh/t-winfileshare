520cbc0b39412d05e00dfcb71a4b8da987d7e0bd
Trinadh <53462309+tr1nadh@users.noreply.github.com>
2023-12-31 20:17:25 +0530

    Initial commit

---
+++ src/test/java/com/example/twinfileshare/service/GoogleDriveServiceTest.java
@@ -0,0 +1,22 @@
+package com.example.twinfileshare.service;
+
+import com.example.twinfileshare.repository.GoogleUserCREDRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class GoogleDriveServiceTest {
+
+    @Autowired
+    private GoogleUserCREDRepository googleUserCREDRepository;
+
+    @Test
+    void uploadFile() {
+        var cred = googleUserCREDRepository.findByEmail("***REMOVED***")
+                .toGoogleCredential();
+        System.out.println(cred);
+    }
+}
