53dce1145ff14d20d50fe3c5009ca14a19a4fb34
Trinadh <53462309+tr1nadh@users.noreply.github.com>
2024-01-02 12:55:18 +0530

    Added the disconnectAccount handle method

--- src/main/java/com/example/twinfileshare/fx/MainPresenter.java
+++ src/main/java/com/example/twinfileshare/fx/MainPresenter.java
@@ -3,9 +3,12 @@ package com.example.twinfileshare.fx;
 import com.example.twinfileshare.fx.controller.MainController;
 import com.example.twinfileshare.fx.service.MainService;
 import jakarta.annotation.PostConstruct;
+import javafx.scene.control.ButtonType;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.security.GeneralSecurityException;
 
 @Component
 public class MainPresenter {
@@ -30,5 +33,30 @@ public class MainPresenter {
                 "Press OK to open the link in default browser.",
                 controller::openAuthLinkInDefaultBrowser
                 );
+    }
+
+    public void handleDisconnectSelectedAccount() {
+        var currentSelectedEmail = controller.getCurrentSelectedEmail();
+        if (!currentSelectedEmail.contains("@")) {
+            fxAlert.informationAlert("Cannot disconnect account",
+                    "Select an email");
+            return;
+        }
+
+        fxAlert.confirmationAlert(
+                "Disconnect Account",
+                "Are sure you want to disconnect the google drive \n" +
+                        "account with email: " + currentSelectedEmail,
+                "",
+                () -> {
+                    try {
+                        controller.disconnectAccount(currentSelectedEmail);
+                    } catch (GeneralSecurityException | IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                },
+                ButtonType.YES,
+                ButtonType.YES, ButtonType.NO
+        );
     }
 }
