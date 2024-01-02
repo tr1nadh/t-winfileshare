acefb58076e6aacd24fca430aae14b6d0cde0dc1
Trinadh <53462309+tr1nadh@users.noreply.github.com>
2024-01-02 12:53:22 +0530

    refactored, add 2 new methods

--- src/main/java/com/example/twinfileshare/fx/FxAlert.java
+++ src/main/java/com/example/twinfileshare/fx/FxAlert.java
@@ -1,10 +1,25 @@
 package com.example.twinfileshare.fx;
 
+import javafx.scene.control.ButtonType;
+
 public interface FxAlert {
     void confirmationAlert(
             String title,
             String header,
             String content,
-            Runnable run
+            Runnable okRun
+    );
+
+    void confirmationAlert(
+            String title,
+            String header,
+            String content,
+            Runnable okRun,
+            ButtonType okButton, ButtonType... buttonTypes
+    );
+
+    void informationAlert(
+            String title,
+            String header
     );
 }
