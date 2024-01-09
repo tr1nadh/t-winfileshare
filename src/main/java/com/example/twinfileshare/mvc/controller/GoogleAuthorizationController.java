309dfbc4425f09e1ef72c89263b172c8396ec289
Trinadh <53462309+tr1nadh@users.noreply.github.com>
2024-01-09 11:25:25 +0530

    Refactored

--- src/main/java/com/example/twinfileshare/mvc/controller/GoogleAuthorizationController.java
+++ src/main/java/com/example/twinfileshare/mvc/controller/GoogleAuthorizationController.java
@@ -1,7 +1,6 @@
 package com.example.twinfileshare.mvc.controller;
 
 import com.example.twinfileshare.service.GoogleAuthorizationService;
-import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
