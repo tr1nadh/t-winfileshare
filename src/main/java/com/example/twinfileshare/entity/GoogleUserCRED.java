bc90f2143e73b323b3f944fa375c777ab6d99499
Trinadh <53462309+tr1nadh@users.noreply.github.com>
2024-01-09 18:59:28 +0530

    Removed unrelavant method

--- src/main/java/com/example/twinfileshare/entity/GoogleUserCRED.java
+++ src/main/java/com/example/twinfileshare/entity/GoogleUserCRED.java
@@ -1,15 +1,7 @@
 package com.example.twinfileshare.entity;
 
-import com.google.api.client.auth.oauth2.BearerToken;
-import com.google.api.client.auth.oauth2.ClientParametersAuthentication;
-import com.google.api.client.auth.oauth2.Credential;
 import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken;
 import com.google.api.client.googleapis.auth.oauth2.GoogleTokenResponse;
-import com.google.api.client.http.GenericUrl;
-import com.google.api.client.http.HttpTransport;
-import com.google.api.client.http.javanet.NetHttpTransport;
-import com.google.api.client.json.JsonFactory;
-import com.google.api.client.json.gson.GsonFactory;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
@@ -42,26 +34,5 @@ public class GoogleUserCRED {
         googleUserCRED.setId(idTokenPayload.getSubject());
 
         return googleUserCRED;
-    }
-
-    private static final HttpTransport HTTP_TRANSPORT = new NetHttpTransport();
-    private static final JsonFactory JSON_FACTORY = GsonFactory.getDefaultInstance();
-
-    public Credential toGoogleCredential() {
-        var cred = new Credential.Builder(BearerToken.authorizationHeaderAccessMethod())
-                .setJsonFactory(JSON_FACTORY)
-                .setTransport(HTTP_TRANSPORT)
-                .setClientAuthentication(new ClientParametersAuthentication(
-                        "${google.oauth2.client.id}",
-                        "${google.oauth2.client.secret}"
-                ))
-                .setTokenServerUrl(new GenericUrl("https://accounts.google.com/o/oauth2/auth"))
-                .build();
-
-        cred.setAccessToken(accessToken);
-        cred.setExpiresInSeconds(expires);
-        cred.setRefreshToken(refreshToken);
-
-        return cred;
     }
 }
