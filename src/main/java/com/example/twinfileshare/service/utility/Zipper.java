a0fa0134f5707d84f1c87a10c92523712add5165
Trinadh <53462309+tr1nadh@users.noreply.github.com>
2024-01-11 13:34:26 +0530

    Initial commit

---
+++ src/main/java/com/example/twinfileshare/service/utility/Zipper.java
@@ -0,0 +1,46 @@
+package com.example.twinfileshare.service.utility;
+
+import com.google.api.client.util.Strings;
+import lombok.extern.log4j.Log4j2;
+import net.lingala.zip4j.ZipFile;
+import net.lingala.zip4j.model.ZipParameters;
+import org.springframework.stereotype.Service;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+@Service
+@Log4j2
+public class Zipper {
+
+    public boolean zipFiles(List<File> files, String zipName) {
+        if (files.isEmpty()) {
+            log.warn("No files to zip");
+            return false;
+        }
+
+        if (Strings.isNullOrEmpty(zipName)) {
+            log.error("Cannot create zip: zip name is empty or null");
+            return false;
+        }
+
+        try (var zip = new ZipFile(zipName + ".zip")) {
+            for (var file : files) {
+                if (file.exists())
+                    zip.addFile(file, createZipParameters(file));
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        return true;
+    }
+
+    private ZipParameters createZipParameters(File file) {
+        var zipParameters = new ZipParameters();
+        zipParameters.setFileNameInZip(file.getName());
+
+        return zipParameters;
+    }
+}
