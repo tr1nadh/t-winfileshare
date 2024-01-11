51f22ebe4772a093c75b87b9e2a7e6434972227d
Trinadh <53462309+tr1nadh@users.noreply.github.com>
2024-01-11 13:34:26 +0530

    Initial commit

---
+++ src/test/java/com/example/twinfileshare/service/utility/ZipperTest.java
@@ -0,0 +1,38 @@
+package com.example.twinfileshare.service.utility;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.io.File;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class ZipperTest {
+
+
+    @Autowired
+    private Zipper zipper;
+
+    @Test
+    void testIfEmptyFileListToZip() {
+        var result = zipper.zipFiles(List.of(), "");
+
+        assertFalse(result);
+    }
+
+    @Test
+    void testAddingListOfFiles() {
+        List<File> files = List.of(
+                new File("testfile-path"),
+                new File("testfile-path"),
+                new File("testfile-path")
+        );
+
+        var result = zipper.zipFiles(files, "test-zip");
+
+        assertTrue(result);
+    }
+}
