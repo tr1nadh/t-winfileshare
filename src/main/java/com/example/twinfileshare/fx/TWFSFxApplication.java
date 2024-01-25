f045bd5e82c0844820d4ce05d307497e9ba6ca5c
Trinadh <53462309+tr1nadh@users.noreply.github.com>
2024-01-25 15:40:00 +0530

    Refactored and added a method to change scene

--- src/main/java/com/example/twinfileshare/fx/TWFSFxApplication.java
+++ src/main/java/com/example/twinfileshare/fx/TWFSFxApplication.java
@@ -12,13 +12,29 @@ import java.io.IOException;
 @Configuration
 public class TWFSFxApplication {
 
-    public static void loadView(Stage stage) throws IOException {
-        FXMLLoader loader = new FXMLLoader(TWFSFxApplication.class.getResource("/templates/fx/Main.fxml"));
+    private static Stage mainStage;
+
+    public static void loadPrimaryView(Stage stage) throws IOException {
+        mainStage = getStageWithProperties(stage);
+        mainStage.setScene(generateScene("/templates/fx/Main.fxml"));
+        mainStage.show();
+    }
+
+    private static Stage getStageWithProperties(Stage stage) {
+        stage.setTitle("T-WinFileShare-v1-alpha");
+        stage.setResizable(false);
+        return stage;
+    }
+
+    private static Scene generateScene(String resource) throws IOException {
+        FXMLLoader loader = new FXMLLoader(TWFSFxApplication.class.getResource(resource));
         loader.setControllerFactory(TWinFileShareApplication.getApplicationContext()::getBean);
         Parent root = loader.load();
-        stage.setTitle("T-WinFileShare-v1-alpha");
-        stage.setScene(new Scene(root));
-        stage.setResizable(false);
-        stage.show();
+        return new Scene(root);
+    }
+
+    public static void loadScene(String resource) throws IOException {
+        mainStage.setScene(generateScene(resource));
+        mainStage.show();
     }
 }
