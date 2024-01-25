e659ae5f0e5944e9e596f04a62dfdd3c8a12c389
Trinadh <53462309+tr1nadh@users.noreply.github.com>
2024-01-25 20:05:24 +0530

    Added addFile and init methods to apply more logic

--- src/main/java/com/example/twinfileshare/fx/HistoryPresenter.java
+++ src/main/java/com/example/twinfileshare/fx/HistoryPresenter.java
@@ -1,6 +1,8 @@
 package com.example.twinfileshare.fx;
 
 import com.example.twinfileshare.fx.view.HistoryView;
+import com.example.twinfileshare.entity.HistoryFile;
+import com.example.twinfileshare.repository.HistoryRepository;
 import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -18,7 +20,18 @@ public class HistoryPresenter {
         view.setPresenter(this);
     }
 
+    @Autowired
+    private HistoryRepository repository;
+
+    public void init() {
+        view.addFilesToListView(repository.findAll());
+    }
+
     public void HandleChangeToMainScene() throws IOException {
         TWFSFxApplication.loadScene("/templates/fx/Main.fxml");
+    }
+
+    public void addFile(HistoryFile historyFile) {
+        view.addFileToListView(historyFile);
     }
 }
