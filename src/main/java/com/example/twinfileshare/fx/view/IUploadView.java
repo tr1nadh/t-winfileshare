7ea9bb421a9ef8349a80c8881bc634c7fdd2332f
Trinadh <53462309+tr1nadh@users.noreply.github.com>
2024-02-02 16:10:14 +0530

    Initial commit

---
+++ src/main/java/com/example/twinfileshare/fx/view/IUploadView.java
@@ -0,0 +1,48 @@
+package com.example.twinfileshare.fx.view;
+
+import com.example.twinfileshare.fx.presenter.UploadPresenter;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
+import javafx.scene.control.SelectionMode;
+import org.springframework.stereotype.Component;
+
+import java.io.File;
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+import java.util.List;
+
+@Component
+public interface IUploadView extends Initializable {
+    void setAccountChoiceBoxItems(List<String> items);
+    void setAccountChoiceBoxValue(String value);
+    void removeItemFromAccountChoiceBox(String item);
+    String getAccountChoiceBoxValue();
+    void disableAccountChoiceBox(boolean disable);
+    ObservableList<String> getAccountChoiceBoxItems();
+    void disableAccountDisconnectBTN(boolean disable);
+    void disableAddFilesBTN(boolean disable);
+    void disableRemoveFilesBTN(boolean disable);
+    void disableClearFilesBTN(boolean disable);
+    void disableHistoryBTN(boolean disable);
+    void setFileListViewSelectionMode(SelectionMode selectionMode);
+    void addItemsToFileListView(List<String> items);
+    ObservableList<String> getFileListViewItems();
+    void clearFileListViewItems();
+    ObservableList<String> getSelectedFileListViewItems();
+    void disableFileListView(boolean disable);
+    void setFileListViewItems(ObservableList<String> items);
+    void setFileUploadProgressBarVisible(boolean visible);
+    void updateFileUploadProgressBar(double value);
+    void setUploadBTNText(String text);
+    List<File> openMultipleFileChooserWindow(String windowTitle, ActionEvent event);
+    String showTextInputDialog(String placeholderText, String title, String header);
+    void setUploadPresenter(UploadPresenter presenter);
+    void connectGoogleDrive(ActionEvent event);
+    void disconnectSelectedAccount(ActionEvent event);
+    void addFilesFromFileManager(ActionEvent event);
+    void removeFilesFromFileListView(ActionEvent event);
+    void clearFileListView(ActionEvent event);
+    void changeToHistoryScene(ActionEvent event) throws IOException;
+    void uploadFiles(ActionEvent event) throws IOException, InterruptedException, GeneralSecurityException;
+}
