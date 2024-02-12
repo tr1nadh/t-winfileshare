0911bbe126c40dcfc1e174bd1af64a4b11939ba5
Trinadh <53462309+tr1nadh@users.noreply.github.com>
2024-02-12 19:08:03 +0530

    Initial commit

---
+++ src/main/java/com/example/twinfileshare/fx/view/MainView.java
@@ -0,0 +1,34 @@
+package com.example.twinfileshare.fx.view;
+
+import com.example.twinfileshare.fx.TWFSFxApplication;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
+import lombok.SneakyThrows;
+import org.springframework.stereotype.Controller;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+@Controller
+public class MainView implements Initializable {
+
+    @FXML
+    private Tab shareVLinkTab;
+
+    @FXML
+    private Tab shareVEmailTab;
+
+    @FXML
+    private Tab manageTab;
+
+    @SneakyThrows
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        shareVLinkTab.setContent(TWFSFxApplication.generateScene("/templates/fx/LinkShare.fxml").getRoot());
+        shareVEmailTab.setContent(TWFSFxApplication.generateScene("/templates/fx/LinkShare.fxml").getRoot());
+        manageTab.setContent(TWFSFxApplication.generateScene("/templates/fx/History.fxml").getRoot());
+    }
+}
