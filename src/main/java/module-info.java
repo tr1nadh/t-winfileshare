62e00009477b715ae64881efa13e3b56817c912a
Trinadh <53462309+tr1nadh@users.noreply.github.com>
2024-02-18 22:05:03 +0530

    Revert "Re-added"
    This reverts commit 713e1b599c7cb164c7d491cb47213feb23ab20da.

---
+++ src/main/java/module-info.java
@@ -0,0 +1,52 @@
+module t.winfileshare {
+    requires javafx.graphics;
+    requires spring.boot;
+    requires spring.boot.autoconfigure;
+    requires spring.context;
+    requires static lombok;
+    requires spring.webmvc;
+    requires jakarta.persistence;
+    requires google.api.client;
+    requires spring.beans;
+    requires javafx.fxml;
+    requires javafx.controls;
+    requires jakarta.annotation;
+    requires com.google.api.client.auth;
+    requires spring.web;
+    requires org.apache.tomcat.embed.core;
+    requires spring.data.jpa;
+    requires spring.tx;
+    requires zip4j;
+    requires com.google.api.client;
+    requires com.google.api.client.json.gson;
+    requires com.google.api.services.drive;
+    requires spring.core;
+    requires thymeleaf;
+    requires org.apache.logging.log4j;
+    requires org.hibernate.orm.core;
+    requires spring.aop;
+    requires spring.boot.starter.aop;
+    requires org.controlsfx.controls;
+    requires spring.boot.starter.web;
+
+    exports com.example.twinfileshare;
+    exports com.example.twinfileshare.service;
+    exports com.example.twinfileshare.service.utility;
+    exports com.example.twinfileshare.listener;
+    exports com.example.twinfileshare.mvc.controller;
+
+    opens com.example.twinfileshare;
+    opens com.example.twinfileshare.service;
+    opens com.example.twinfileshare.config;
+    opens com.example.twinfileshare.entity;
+    opens com.example.twinfileshare.listener;
+    opens com.example.twinfileshare.mvc.controller;
+    opens com.example.twinfileshare.fx;
+    opens com.example.twinfileshare.fx.view;
+    opens com.example.twinfileshare.fx.model;
+    opens com.example.twinfileshare.fx.alert;
+    exports com.example.twinfileshare.exception;
+    opens com.example.twinfileshare.exception;
+    exports com.example.twinfileshare.entity;
+    opens com.example.twinfileshare.fx.presenter;
+}
