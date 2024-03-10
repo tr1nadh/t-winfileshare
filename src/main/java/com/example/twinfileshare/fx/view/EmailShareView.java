3921ad474b032e2cefc3a37f2dd7c446c3c5ed4d
Trinadh <53462309+tr1nadh@users.noreply.github.com>
2024-03-10 10:02:50 +0530

    Added functional code in EmailShareView.java

--- src/main/java/com/example/twinfileshare/fx/view/EmailShareView.java
+++ src/main/java/com/example/twinfileshare/fx/view/EmailShareView.java
@@ -1,7 +1,90 @@
 package com.example.twinfileshare.fx.view;
 
+import com.example.twinfileshare.fx.presenter.EmailSharePresenter;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
 import org.springframework.stereotype.Controller;
 
+import javax.mail.MessagingException;
+import java.io.IOException;
+import java.net.URL;
+import java.util.List;
+import java.util.ResourceBundle;
+
 @Controller
-public class EmailShareView {
+public class EmailShareView implements Initializable {
+
+    @FXML
+    private ChoiceBox<String> fromAccountChoiceBox;
+
+    @FXML
+    private TextArea titleText;
+
+    @FXML
+    private TextArea toText;
+
+    @FXML
+    private TextArea messageBodyText;
+
+    @FXML
+    private Button addFilesBTN;
+
+    @FXML
+    private Button removeFilesBTN;
+
+    @FXML
+    private Button clearFilesBTN;
+
+    @FXML
+    private ListView<String> fileListView;
+
+    @FXML
+    private Button sendBTN;
+
+    @FXML
+    private ProgressBar fileUploadProgressBar;
+
+    @FXML
+    private Button manageAccounts;
+
+    private EmailSharePresenter emailSharePresenter;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        emailSharePresenter.init();
+    }
+
+    public void setFromAccountChoiceBoxItems(List<String> items) {
+        fromAccountChoiceBox.getItems().addAll(items);
+    }
+
+    public void manageAccounts(ActionEvent event) {
+        emailSharePresenter.handleManageAccounts(event);
+    }
+
+    public void sendAndShare(ActionEvent event) throws MessagingException, IOException {
+        emailSharePresenter.handleSendAndShare();
+    }
+
+    public void setEmailSharePresenter(EmailSharePresenter emailSharePresenter) {
+        this.emailSharePresenter = emailSharePresenter;
+    }
+
+    public String getSelectedFromAccountChoiceBoxItem() {
+        return fromAccountChoiceBox.getValue();
+    }
+
+    public String getToTextValue() {
+        return toText.getText();
+    }
+
+    public String getTitleTextValue() {
+        return titleText.getText();
+    }
+
+    public String getMessageBodyTextValue() {
+        return messageBodyText.getText();
+    }
 }
