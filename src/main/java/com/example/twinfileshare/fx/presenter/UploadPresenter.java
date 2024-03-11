44c062ce5e53565b7124f7961ebfdf3c1d885695
Trinadh <53462309+tr1nadh@users.noreply.github.com>
2024-03-11 13:07:47 +0530

    Refactored and fixed the upload cancelled issue

--- src/main/java/com/example/twinfileshare/fx/presenter/UploadPresenter.java
+++ src/main/java/com/example/twinfileshare/fx/presenter/UploadPresenter.java
@@ -1,34 +1,43 @@
 package com.example.twinfileshare.fx.presenter;
 
-import com.example.twinfileshare.event.payload.UploadCancelledEvent;
 import com.example.twinfileshare.fx.TWFSFxApplication;
 import com.example.twinfileshare.fx.view.UploadView;
 import jakarta.annotation.PostConstruct;
-import javafx.event.ActionEvent;
 import javafx.scene.Scene;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
+import lombok.Setter;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 
+@Component()
+@Scope(value = ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 public class UploadPresenter {
 
     @Autowired
     private UploadView uploadView;
 
-    private final Stage uploadStage = new Stage();
+    private Stage uploadStage;
 
     private boolean isUploadActive;
 
-    public void start(Stage stage) {
+    @Setter
+    private Runnable cancellable;
+
+    public void start(Stage owner) {
+        this.uploadStage = new Stage();
         isUploadActive = true;
+
         try {
             Scene scene = TWFSFxApplication.generateScene("/templates/fx/UploadFiles.fxml");
             uploadStage.setScene(scene);
-            uploadStage.initModality(Modality.WINDOW_MODAL);
-            uploadStage.initOwner(stage);
+            uploadStage.initModality(Modality.APPLICATION_MODAL);
+            uploadStage.setMaximized(false);
+            uploadStage.initOwner(owner);
             uploadStage.show();
         } catch (IOException e) {
             e.printStackTrace();
@@ -43,13 +52,9 @@ public class UploadPresenter {
         uploadView.setUploadLogText(value);
     }
 
-    @Autowired
-    private ApplicationEventPublisher eventPublisher;
-
-    public void handleCancelUpload(ActionEvent event) {
-        eventPublisher.publishEvent(new UploadCancelledEvent(this));
-
-        close();
+    public void handleCancelUpload() {
+        if (cancellable != null) cancellable.run();
+        closeWindow();
     }
 
     public boolean isUploadActive() {
@@ -61,7 +66,7 @@ public class UploadPresenter {
         uploadView.setUploadPresenter(this);
     }
 
-    public void close() {
+    public void closeWindow() {
         isUploadActive = false;
 
         uploadStage.close();
