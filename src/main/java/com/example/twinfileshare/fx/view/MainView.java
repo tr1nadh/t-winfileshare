f98dd760442ccaf3f7c5ba73fd60e08aa769dc93
Trinadh <53462309+tr1nadh@users.noreply.github.com>
2024-03-12 12:36:31 +0530

    Added account manage to main view

--- src/main/java/com/example/twinfileshare/fx/view/MainView.java
+++ src/main/java/com/example/twinfileshare/fx/view/MainView.java
@@ -4,6 +4,7 @@ import com.example.twinfileshare.fx.TWFSFxApplication;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Tab;
+import javafx.scene.layout.AnchorPane;
 import lombok.SneakyThrows;
 import org.springframework.stereotype.Controller;
 
@@ -22,11 +23,15 @@ public class MainView implements Initializable {
     @FXML
     private Tab manageTab;
 
+    @FXML
+    private AnchorPane manageAccountPane;
+
     @SneakyThrows
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         shareVLinkTab.setContent(TWFSFxApplication.generateScene("/templates/fx/LinkShare.fxml").getRoot());
         shareVEmailTab.setContent(TWFSFxApplication.generateScene("/templates/fx/EmailShare.fxml").getRoot());
         manageTab.setContent(TWFSFxApplication.generateScene("/templates/fx/Manage.fxml").getRoot());
+        manageAccountPane.getChildren().setAll(TWFSFxApplication.generateScene("/templates/fx/AccountManage.fxml").getRoot());
     }
 }
