32a4879cbd2d3fa1d640bb73e510c23ad2d3a94f
Trinadh <53462309+tr1nadh@users.noreply.github.com>
2024-03-14 20:30:19 +0530

    Fixed: when upload progress dialog closed now it will ask confirmation

--- src/main/java/com/example/twinfileshare/fx/presenter/UploadProgressPresenter.java
+++ src/main/java/com/example/twinfileshare/fx/presenter/UploadProgressPresenter.java
@@ -4,8 +4,11 @@ import com.example.twinfileshare.fx.TWFSFxApplication;
 import com.example.twinfileshare.fx.view.UploadProgressView;
 import jakarta.annotation.PostConstruct;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
 import lombok.Setter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -27,6 +30,7 @@ public class UploadProgressPresenter {
 
     public void start(Stage owner) {
         this.uploadStage = new Stage();
+        uploadStage.setOnCloseRequest(this::handleOnCloseRequest);
         isUploadActive = true;
 
         try {
@@ -39,6 +43,22 @@ public class UploadProgressPresenter {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    private void handleOnCloseRequest(WindowEvent windowEvent) {
+        uploadStage.hide();
+        var alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setResizable(false);
+        alert.setTitle("Cancellation alert");
+        alert.setHeaderText("Are you sure you want to cancel?");
+        alert.showAndWait()
+                .filter(res -> res == ButtonType.OK)
+                .ifPresentOrElse(this::onCloseRequest,
+                        () -> uploadStage.showAndWait());
+    }
+
+    private void onCloseRequest(ButtonType buttonType) {
+        handleCancelUpload();
     }
 
     public void updateProgress(double progress) {
