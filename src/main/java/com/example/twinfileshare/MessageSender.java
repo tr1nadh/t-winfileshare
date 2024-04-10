c538f678013140c3fdc3896c5dcd8d04845d8bf0
trinadh <53462309+tr1nadh@users.noreply.github.com>
2024-04-10 18:43:21 +0530

    Refactored

--- src/main/java/com/example/twinfileshare/MessageSender.java
+++ src/main/java/com/example/twinfileshare/MessageSender.java
@@ -2,6 +2,7 @@ package com.example.twinfileshare;
 
 import com.example.twinfileshare.repository.GoogleUserCREDRepository;
 import com.example.twinfileshare.service.GoogleAuthorizationService;
+import com.google.api.client.auth.oauth2.Credential;
 import com.google.api.client.googleapis.json.GoogleJsonError;
 import com.google.api.client.googleapis.json.GoogleJsonResponseException;
 import com.google.api.client.http.javanet.NetHttpTransport;
@@ -36,21 +37,45 @@ public class MessageSender {
     public Message sendEmail(String fromEmailAddress, String toEmailAddress,
                              String messageSubject, String bodyText, String link)
             throws MessagingException, IOException {
-        var googleUserCRED = googleUserCREDRepository.findByEmail(fromEmailAddress);
-        var credential = googleAuthorizationService.toGoogleCredential(googleUserCRED);
+        var credential = getCredential(fromEmailAddress);
+        Gmail service = getGmail(credential);
 
-        // Create the gmail API client
-        Gmail service = new Gmail.Builder(new NetHttpTransport(),
-                GsonFactory.getDefaultInstance(),
-                credential)
-                .setApplicationName(googleClientAppName)
-                .build();
+        var email = getMimeMessage(fromEmailAddress, toEmailAddress, messageSubject, bodyText, link);
 
+        var message = getMessage(email);
+
+        try {
+            message = service.users().messages().send("me", message).execute();
+            System.out.println("Message id: " + message.getId());
+            System.out.println(message.toPrettyString());
+            return message;
+        } catch (GoogleJsonResponseException e) {
+            GoogleJsonError error = e.getDetails();
+            if (error.getCode() == 403) {
+                System.err.println("Unable to send message: " + e.getDetails());
+            } else {
+                throw e;
+            }
+        }
+
+        return null;
+    }
+
+    private Message getMessage(MimeMessage email) throws IOException, MessagingException {
+        ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+        email.writeTo(buffer);
+        byte[] rawMessageBytes = buffer.toByteArray();
+        String encodedEmail = Base64.encodeBase64URLSafeString(rawMessageBytes);
+        Message message = new Message();
+        message.setRaw(encodedEmail);
+        return message;
+    }
+
+    private MimeMessage getMimeMessage(String fromEmailAddress, String toEmailAddress, String messageSubject, String bodyText, String link) throws MessagingException {
         bodyText = bodyText + "<br> <br> " +
                 "<a href='" + link + "'>Click here to download" +
                 "</a>";
 
-        // Encode as MIME message
         Properties props = new Properties();
         Session session = Session.getDefaultInstance(props, null);
         MimeMessage email = new MimeMessage(session);
@@ -59,30 +84,21 @@ public class MessageSender {
                 new InternetAddress(toEmailAddress));
         email.setSubject(messageSubject);
         email.setContent(bodyText, "text/html; charset=utf-8");
+        return email;
+    }
 
-        // Encode and wrap the MIME message into a gmail message
-        ByteArrayOutputStream buffer = new ByteArrayOutputStream();
-        email.writeTo(buffer);
-        byte[] rawMessageBytes = buffer.toByteArray();
-        String encodedEmail = Base64.encodeBase64URLSafeString(rawMessageBytes);
-        Message message = new Message();
-        message.setRaw(encodedEmail);
+    private Credential getCredential(String fromEmailAddress) {
+        var googleUserCRED = googleUserCREDRepository.findByEmail(fromEmailAddress);
+        var credential = googleAuthorizationService.toGoogleCredential(googleUserCRED);
+        return credential;
+    }
 
-        try {
-            // Create send message
-            message = service.users().messages().send("me", message).execute();
-            System.out.println("Message id: " + message.getId());
-            System.out.println(message.toPrettyString());
-            return message;
-        } catch (GoogleJsonResponseException e) {
-            // TODO(developer) - handle error appropriately
-            GoogleJsonError error = e.getDetails();
-            if (error.getCode() == 403) {
-                System.err.println("Unable to send message: " + e.getDetails());
-            } else {
-                throw e;
-            }
-        }
-        return null;
+    private Gmail getGmail(Credential credential) {
+        Gmail service = new Gmail.Builder(new NetHttpTransport(),
+                GsonFactory.getDefaultInstance(),
+                credential)
+                .setApplicationName(googleClientAppName)
+                .build();
+        return service;
     }
 }
